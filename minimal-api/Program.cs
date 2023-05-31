e0052ebebb8535b7b5c0b589566430d9eab63707
Filipe Pedro Silva <48092050+PedrooFilipe@users.noreply.github.com>
2023-05-30 22:22:43 -0300

    first commit

---
+++ minimal-api/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/users/{userId}/books/{bookId}", 
+    (int userId, int bookId) => $"The user id is {userId} and book id is {bookId}");
+
+app.Run();
